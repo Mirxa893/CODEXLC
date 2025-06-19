c8aaab47cfe5fa3b585e913f57ecfcdf47512495
Mirxa893 <mirxakamran893@gmail.com>
2025-06-19 18:23:03 +0500

    Update chat-panel.tsx

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -1,24 +1,3 @@
-import { type UseChatHelpers } from 'ai/react'
-
-import { ButtonScrollToBottom } from '@/components/button-scroll-to-bottom'
-import { PromptForm } from '@/components/prompt-form'
-import { Button } from '@/components/ui/button'
-import { IconRefresh, IconStop } from '@/components/ui/icons'
-
-export interface ChatPanelProps
-  extends Pick<
-    UseChatHelpers,
-    | 'append'
-    | 'isLoading'
-    | 'reload'
-    | 'messages'
-    | 'stop'
-    | 'input'
-    | 'setInput'
-  > {
-  id?: string
-}
-
 export function ChatPanel({
   id,
   isLoading,
@@ -30,9 +9,10 @@ export function ChatPanel({
   messages
 }: ChatPanelProps) {
   return (
-    <div className="fixed inset-x-0 bottom-0 bg-gradient-to-b from-muted/10 from-10% to-muted/30 to-50%">
+    <div className="fixed inset-x-0 bottom-0 bg-gradient-to-b from-muted/10 from-10% to-muted/30 to-50% z-10">
       <ButtonScrollToBottom />
-      <div className="mx-auto sm:max-w-2xl sm:px-4">
+      
+      <div className="mx-auto w-full max-w-5xl px-4">
         <div className="flex h-10 items-center justify-center">
           {isLoading ? (
             <Button
@@ -56,6 +36,7 @@ export function ChatPanel({
             )
           )}
         </div>
+
         <div className="space-y-4 border-t bg-background px-4 py-2 shadow-lg sm:rounded-t-xl sm:border md:py-4">
           <PromptForm
             onSubmit={async value => {
