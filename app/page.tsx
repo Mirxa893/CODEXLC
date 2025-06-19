e0c48b7e567a9dda0559b101057b183376612d75
Mirxa893 <mirxakamran893@gmail.com>
2025-06-19 13:13:25 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { nanoid } from 'nanoid/non-secure'
+import { nanoid } from 'nanoid/non-secure' // Safe for Edge runtime
 import { Chat } from '@/components/chat'
 
 export const runtime = 'edge'
 
-export default function IndexPage() {
+export default function Page() {
   const id = nanoid()
-
   return <Chat id={id} />
 }
