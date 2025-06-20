10aad2f1a9460d536172a6077875927baf426c0e
Mirxa893 <mirxakamran893@gmail.com>
2025-06-20 19:09:49 +0500

    Update page.tsx

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,13 +1,9 @@
-// /app/chat/[id]/page.tsx
-
-'use client'
-
-import { useParams } from 'next/navigation'
 import { Chat } from '@/components/chat'
 
-export default function ChatPage() {
-  const params = useParams()
-  const chatId = Array.isArray(params.id) ? params.id[0] : params.id
+interface ChatPageProps {
+  params: { id: string }
+}
 
-  return <Chat id={chatId} />
+export default function ChatPage({ params }: ChatPageProps) {
+  return <Chat id={params.id} />
 }
