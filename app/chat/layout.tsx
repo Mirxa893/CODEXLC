ead512cf54e1a728b28d6f75a9bc31c2f11217a5
Mirxa893 <mirxakamran893@gmail.com>
2025-06-20 15:12:16 +0500

    Update layout.tsx

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -3,7 +3,6 @@ import { Toaster } from 'react-hot-toast'
 import { fontMono, fontSans } from '@/lib/fonts'
 import { cn } from '@/lib/utils'
 import { Providers } from '@/components/providers'
-import { Header } from '@/components/header'
 import { Sidebar } from '@/components/Sidebar'
 
 export const metadata = {
@@ -16,20 +15,9 @@ export const metadata = {
 
 export default function ChatLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body className={cn('font-sans antialiased', fontSans.variable, fontMono.variable)}>
-        <Toaster />
-        <Providers attribute="class" defaultTheme="system" enableSystem>
-          <div className="flex min-h-screen md:flex-row">
-            <Sidebar />
-            <div className="flex flex-col flex-1">
-              <Header />
-              <main className="flex-1 p-4 bg-muted/50">{children}</main>
-            </div>
-          </div>
-        </Providers>
-      </body>
-    </html>
+    <div className="flex min-h-screen md:flex-row">
+      <Sidebar />
+      <main className="flex-1 p-4 bg-muted/50">{children}</main>
+    </div>
   )
 }
