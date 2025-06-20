2a1ad6bbe48e6b735c8716c3c5172d0e5dd7e0c6
Mirxa893 <mirxakamran893@gmail.com>
2025-06-20 16:12:56 +0500

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,7 +6,7 @@ import { useChatMessages } from '@/lib/hooks/use-chat-messages'
 
 export function Sidebar() {
   const { isOpen, closeSidebar } = useSidebar()
-  const { chat } = useChatMessages()
+  const { Chat } = useChatMessages()
 
   return (
     <>
